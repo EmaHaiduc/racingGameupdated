7b29cd28a821b2827e21e21ad4638365ec0a8b4f
EmaHaiduc <emma.pasca@yahoo.com>
2017-11-23 20:39:24 +0200

    simplified App main

--- src/main/java/org/fasttrackit/Race.java
+++ src/main/java/org/fasttrackit/Race.java
@@ -2,8 +2,38 @@ package org.fasttrackit;
 
 public class Race {
 private Track track;
- private Car firstCompetitor;
+private Car firstCompetitor;
 private Car secondCompetitor;
+
+public void start() {
+    Engine engine = new Engine();
+    engine.setManufacturer("Volvo");
+    firstCompetitor= createCompetitor(engine, "Volvo", "red",40,8.5,4);
+    Engine secondengine = new Engine();
+    secondengine.setManufacturer("Toyota");
+    secondCompetitor= createCompetitor(engine, "Toyota", "black",40,10.5,4);
+
+}
+
+
+// parameters contain car prefix just to demo they can have any name
+private Car createCompetitor(Engine carEngine, String carName, String carColor,double carFuelLevel, double carMileage, int carDoorCount){
+    Car competitor = new Car (carEngine);
+    competitor.setName(carName);
+    competitor.setColor(carColor);
+    competitor.setFuelLevel(carFuelLevel);
+    competitor.setDoorCount(carDoorCount);
+
+    return competitor;
+}
+
+
+
+
+
+
+
+
 
     public Track getTrack() {
         return track;
