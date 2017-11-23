90a93e90699e87a6cf94451bd1c0f81b7d5b7b47
EmaHaiduc <emma.pasca@yahoo.com>
2017-11-23 19:34:18 +0200

    project structure implemented incapsulation for all classes

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -5,26 +5,26 @@ public class App {
     public static void main( String[] args ) {
         Race race = new Race();
         Track track = new Track();
-        track.lenght = 100.5;
-        race .track = track;
+        track.setLenght(100);
+        race.setTrack(track);
         Engine engine = new Engine();
-        engine.manufacturer = "Volvo";
+        engine.setManufacturer("Volvo");
         Car car1 = new Car (engine);
         car1.setName("Volvo");
         car1.setColor ("black");
         car1.setFuelLevel (100.00);
         car1.setMileage(12.5);
-        car1.doorCount = 4;
+        car1.setDoorCount(4);
 
         Engine engine2 = new Engine();
-        engine2.manufacturer = "honda";
+        engine2.setManufacturer("honda");
 
         Car car2 = new Car(engine2);
         car2.setName ("Honda");
         car2.setColor ("red");
         car2.setFuelLevel(100.00);
         car2.setMileage (13.5);
-        car2.doorCount = 2;
+        car2.setDoorCount(2);
         Vehicle vehicle= new Vehicle();
         vehicle.accelerate (20.5);
         AutoVehicle autovehicle= new AutoVehicle (new Engine());
